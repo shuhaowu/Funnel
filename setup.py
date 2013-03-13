a7efad159b59e91f1449d530fa061a7d04451862
Shuhao <shuhao@shuhaowu.com>
2013-03-13 12:13:37 -0400

    Updated generation script, setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="Funnel",
+      version="0.1",
+      description="A simple static website/blog generator.",
+      author="Shuhao Wu",
+      author_email="shuhao@shuhaowu.com",
+      license="GPL",
+      url="https://github.com/shuhaowu/funnel",
+      py_modules=["funnel"],
+      scripts=["funnel"],
+      install_requires=["Flask", "markdown"],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Unix",
+        "Topic :: Internet",
+        "Environment :: Console"
+      ]
+     )
